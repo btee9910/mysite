e15f39578258c071dc6b35159488b00f3b8126e6
btee9910 <btee9910@gmail.com>
2023-09-21 14:37:47 +1000

    update qualification section

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
       <Home />
       <About />
       <Skills />
-      <Portfolio />
       <Qualification />
+      <Portfolio />
       <Contact />
     </>
   )
--- src/components/Qualification/TimelineBlock.jsx
+++ src/components/Qualification/TimelineBlock.jsx
@@ -1,6 +1,9 @@
+import { useState, useRef } from 'react';
 import './TimelineBlock.css';
 
 const TimelineBlock = ({ institution, title, period, description, index }) => {
+    const [hover, setHover] = useState(false)
+    const descriptionRef = useRef()
 
     const DisplayBlock = () => {
         return (
@@ -12,24 +15,32 @@ const TimelineBlock = ({ institution, title, period, description, index }) => {
         )
     }
 
+    const DescriptionBlock = () => {
+        return (
+            <>
+                {hover && <p className='timeline__description' >{description}</p>}
+            </>
+        )
+    }
+
     let firstBlock = <DisplayBlock />;
-    let secondBlock = <></>;
+    let secondBlock = <DescriptionBlock />;
 
     if (index % 2 === 1) {
-        firstBlock = <></>;
+        firstBlock = <DescriptionBlock />;
         secondBlock = <DisplayBlock />;
     }
 
-
     return (
-        <div className='timeline__block'>
-            <div className="timeline__subblock timeline__left">
+        <div className='timeline__block' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+            <div className="timeline__subblock timeline__left" >
                 {firstBlock}
             </div>
             <div className="timeline">
-                |
+                <span className='timeline__pin'>⦿</span>
+                <span className='timeline__line'></span>
             </div>
-            <div className="timeline__subblock timeline__right">
+            <div className="timeline__subblock timeline__right" >
                 {secondBlock}
             </div>
         </div>
