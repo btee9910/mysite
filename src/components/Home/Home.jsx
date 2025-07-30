d695c221fc7aac537f24fba34cdc31b91e03eab6
Brandon Tee <128451789+btee9910@users.noreply.github.com>
2025-07-30 17:47:51 +1000

    update title

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,28 +1,35 @@
-import './Home.css'
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <section className='home section' id='home'>
-            <div className="container">
-                <div className="home__subcontainer">
-                    <div className='home__intro__wrapper'>
-                        <h1>Brandon Tee</h1>
-                        <h5>Full Stack Software Developer with a background as Mechanical Design Engineer.</h5>
-                        <h5>Passionate about badminton, photography, indoor plant and technology.</h5>
-                    </div>
-                    <div className='home__button__wrapper'>
-                        <a href="#portfolio" className='home__button btn'>View Projects</a>
-                    </div>
-                </div>
-                <div className="home__subcontainer">
-                    <div className="home__background">
-                        <div className='home__img'></div>
-                    </div>
-                </div>
-            </div>
-        </section>
+  return (
+    <section className="home section" id="home">
+      <div className="container">
+        <div className="home__subcontainer">
+          <div className="home__intro__wrapper">
+            <h1>Brandon Tee</h1>
+            <h5>
+              Software Developer | Former Mechanical Design Engineer | Proud
+              Father of One
+            </h5>
+            <h5>
+              Passionate about technology, badminton, photography, and indoor
+              plants.
+            </h5>
+          </div>
+          <div className="home__button__wrapper">
+            <a href="#portfolio" className="home__button btn">
+              View Projects
+            </a>
+          </div>
+        </div>
+        <div className="home__subcontainer">
+          <div className="home__background">
+            <div className="home__img"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-    )
-}
-
-export default Home
+export default Home;
